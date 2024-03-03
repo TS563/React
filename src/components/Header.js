30b9b2bbe4b10f183ed3fae332069547b5b0a4e1
TS563 <89252926+TS563@users.noreply.github.com>
2024-03-03 17:25:26 +0530

    Add files via upload

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+// components/Header.js
+
+import React from 'react';
+
+function Header() {
+  return (
+    <header>
+      {/* Header content goes here */}
+    </header>
+  );
+}
+
+export default Header;
